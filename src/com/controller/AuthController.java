bbde254e7376412a7090457924126239b4449026
lenovo <lenovo@Inso>
2018-09-06 16:45:24 +0900

    끝!!!

--- src/com/controller/AuthController.java
+++ src/com/controller/AuthController.java
@@ -41,13 +41,14 @@ public class AuthController {
 		
 		System.out.println(map);
 		MemberVo vo = memberDao.findByEmailAndPass(map);
-		if(auth.map.containsKey(vo.getEmail())) {
-			((HttpSession) auth.map.get(vo.getEmail())).setAttribute("auth", null);;
-			auth.map.put(vo.getEmail(), session);
-		}else {
-			auth.map.put(vo.getEmail(), session);
+		if(vo != null) {
+			if(auth.map.containsKey(vo.getEmail())) {	// 중복로그인 방지
+				((HttpSession) auth.map.get(vo.getEmail())).setAttribute("auth", null);;
+				auth.map.put(vo.getEmail(), session);
+			}else {
+				auth.map.put(vo.getEmail(), session);
+			}
 		}
-
 		
 		if(vo != null) {
 			session.setAttribute("auth", vo);	// 로그인 성공하면 세션에 넣어두기
